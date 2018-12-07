a45f0a294aeac92c9d5df269c84f0fdde4e89383
Rajmund Waszkiel <rajmund.wu@gmail.com>
2018-12-07 20:41:46 +0100

    Updates

---
+++ precache-manifest.0fe2d43021e97b9b67af890875a1d30a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c2c3576ff6c924fc423",
+    "url": "/Bouncing_ball/static/js/runtime~main.8c2c3576.js"
+  },
+  {
+    "revision": "1bf77ce09f88f6060e8e",
+    "url": "/Bouncing_ball/static/js/main.1bf77ce0.chunk.js"
+  },
+  {
+    "revision": "ab3c0db7d45dc1e150bc",
+    "url": "/Bouncing_ball/static/js/1.ab3c0db7.chunk.js"
+  },
+  {
+    "revision": "1bf77ce09f88f6060e8e",
+    "url": "/Bouncing_ball/static/css/main.4a55a160.chunk.css"
+  },
+  {
+    "revision": "d5488f566cf586db904d1955b8b08a2f",
+    "url": "/Bouncing_ball/index.html"
+  }
+];
